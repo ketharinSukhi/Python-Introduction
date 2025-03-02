5069a490b19a3d9650dda4e6aa090ee28c5382af
ketharinSukhi <71124727+ketharinSukhi@users.noreply.github.com>
2025-03-02 22:09:37 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""x=float(input("what's x "))
+y=float(input("what's y "))
+# print("sum",x+y)
+z = round(x + y)
+print(f"{z:,}")
+#f=round(x/y)
+f=(x/y)
+print(f"{f:.2f}")
+
+#print(int(input("what's x ")) + int(input("what's y ")))"""
+
+def main():
+    x=int(input("what's x "))
+    print("x squared is",square(x))
+
+def square(n):
+    return pow(n, 2)
+main()
+
---
+++ hello.py
@@ -0,0 +1,30 @@
+# x=int(input())
+# y=int(input())
+# print("hello, world",x+y)
+
+# remove whitespace from str
+#name= name.strip().title()
+#Capitalize user's name and capitalization
+# name=name.title()
+
+"""name=input("what's your name?",).strip().title()
+print(f"hello, world, {name}")
+
+#print(name)
+#print(*objects, sep='', end='\n')
+
+#function create
+def hello(to="world"):
+    print("hello,", to)
+hello()
+name = input("what's your name? ")
+#hello(name)
+hello(name)"""
+
+def main():
+    name = input("what's your name? ")
+    hello()
+def hello(to="world"):
+    print("hello,", to)
+main()
+
