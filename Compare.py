fb8ca1a43facda402169035076d95477ac66d5e3
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-03 09:39:59 +0600

    added new file

--- Compare.py
+++ Compare.py
@@ -1,4 +1,4 @@
-x=int(input("what's x "))
+"""x=int(input("what's x "))
 y=int(input("what's y "))
 
 if x < y:
@@ -6,4 +6,16 @@ if x < y:
 elif x > y:
     print("x is greater than y")
 else:
-    print("x is equal to y") 
+    print("x is equal to y") """
+
+score = int(input("score : "))
+if score >= 90 and score <= 100:
+    print("Grade:A")
+elif score >= 80 and score < 90:
+    print("Grade:B")
+elif score >= 70 and score < 80:
+    print("Grade:C")
+elif score >= 60 and score < 70:
+    print("Grade:D")
+else:
+    print("Grade:F")
