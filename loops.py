29908ccc1fc5d28c580e6df6aa9f89ec0355bbf0
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-03 15:11:08 +0600

    added new file

--- loops.py
+++ loops.py
@@ -36,7 +36,7 @@ for i in range(len(students)):
     print(i+1, students[i])"""
 
 #dict
-students = [
+"""students = [
             {"name":"Hermione","house":"Gryffindor","patronus":"Otter"},
             {"name":"Harry","house":"Gryffindor","patronus":"Stag"},
             {"name":"Ron","house":"Gryffindor","patronus":"Jack Russel Terrier"},
@@ -44,8 +44,24 @@ students = [
              ]
 
 for student in students:
-  print(student["name"], student["house"], student["patronus"], sep=" ----> ")
+  print(student["name"], student["house"], student["patronus"], sep=" ----> ")"""
 
 
+def main() :
+    print_square(3)
 
+
+def print_square(size) :
+    #For each row in square
+    for i in range(size):
+        #For each brick in row
+        #for j in range(size):
+           #print brick
+           #print("*",end="")
+        print_row(size)
+
+def print_row(width):
+    print("*"* width)
+
+main()
 
---
+++ number.py
@@ -0,0 +1,5 @@
+try :
+    x=int(input("what's x? "))
+    print(f"x is {x}")
+except:
+    print("x is not integer")
