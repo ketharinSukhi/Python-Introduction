5fed174879dd1492f58f47bad77b35afcdd42682
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-03 12:03:16 +0600

    added new file

--- loops.py
+++ loops.py
@@ -5,12 +5,27 @@ while i<4:
  print("hello")
  i+=1"""
 
-while True:
+"""while True:
   n = int(input("what's n? "))
   if n>0:
     break
 for _ in range(n):
-  print("hello")
+  print("hello")"""
+
+def main():
+    number = get_number()
+    hello(number)
+
+def get_number():
+    while True:
+        n = int(input("what's n?"))
+        if n > 0:
+            break
+    return n
+def hello(n):
+    for _ in range(n):
+        print("hello")
+main()
 
 
 
