d86ea16ff86f84fc53db0fba46de13ac255271a8
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-03 14:49:14 +0600

    added new file

--- Mario.py
+++ Mario.py
@@ -1,8 +1,17 @@
 def main() :
-    print_row(4)
+    print_square(3)
 
 
-def print_row(width) :
-    print("*" * width)
+def print_square(size) :
+    #For each row in square
+    for i in range(size):
+        #For each brick in row
+        #for j in range(size):
+           #print brick
+           #print("*",end="")
+        print_row(size)
+
+def print_row(width):
+    print("*"* width)
 
 main()
