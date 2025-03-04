1ef92bc9674420aac91528bcba00cbe53b046560
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-04 14:56:00 +0600

    added new file

--- names.py
+++ names.py
@@ -15,8 +15,23 @@ with open("names.txt","a") as file:
  file.write(f"{name}\n")
 file.close()"""
 
-with open("names.txt","r") as file:
-    lines = file.readlines()
+"""with open("names.txt","r") as file:
+    for line in file:
+      print("hello, " ,line.rstrip())"""
 
-for line in lines:
-    print("hello, " ,line)
+#sorted name
+"""names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")"""
+
+#
+with open("names.txt") as file:
+    for line in sorted(file):
+        print("hello, ", line.rstrip()
+              )
+
