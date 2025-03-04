cb14920e443cf21e2f4385318548dd602f7aa4a2
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-04 10:25:35 +0600

    added new file

--- say.py
+++ say.py
@@ -6,11 +6,21 @@ if len(sys.argv) == 2:
     cowsay.trex("hello, " + sys.argv[1])"""
 
 #requests
-import json
+"""import json
 import requests
 import sys
 
 if len(sys.argv) != 2:
     sys.exit()
-response=requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+response=requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+#print(json.dumps(response.json(), indent=2))
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])"""
+
+import sys
+from module import hello # type: ignore
+
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
