92b5e8b87d0ea972e4ac16e9a3f23d3a03dd2f53
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-04 15:34:17 +0600

    added new file

--- students.py
+++ students.py
@@ -1,4 +1,20 @@
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+        #row = line.rstrip().split(",")
+        #print(f"{row[0]} is in {row[1]}")
+
+#phrases
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name":name, "house":house}
+        student["name"]=name
+        student["house"]=house
+        students.append(student)
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
