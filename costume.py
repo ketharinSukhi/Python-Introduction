794fd0c3d7aee4951da9c2168a1931b239ac1405
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-05 11:02:06 +0600

    added new file

--- costume.py
+++ costume.py
@@ -8,5 +8,5 @@ for arg in sys.argv[1:]:
     images.append(image)
 
 image [0].save(
-    "costumes.gif", save_all=True, append_images=[images[1]], duration=200, loop=0
+    "costume.gif", save_all=True, append_images=[images[1]], duration=200, loop=0
 )
