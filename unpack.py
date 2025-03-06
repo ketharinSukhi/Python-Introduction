94e6e8492f75f8a00d89e27d84cba648971ed020
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-06 16:25:23 +0600

    added new file

--- unpack.py
+++ unpack.py
@@ -1,4 +1,6 @@
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
-print(total(100, 50, 25), "Knuts")
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+print(total(**coins), "Knuts") #unpacking
