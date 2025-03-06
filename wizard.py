56151d6419281cce3d9a17cd13493c9defd76d08
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-06 12:44:39 +0600

    added new file

---
+++ wizard.py
@@ -0,0 +1,35 @@
+#print(type({}))
+"""import random
+class Hat:
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw","Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+Hat.sort("Harry")"""
+
+
+class Wizard:
+    def __init__(self, name):
+         if not name:
+            raise ValueError("Missing name")
+         self.name = name
+    
+    ...
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
+        self.house = house
+
+    ...
+
+class Professor(Wizard):
+    def __int__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    ...
+wizard = Wizard("Albus")        
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus","Defense Against the Dark Arts")
