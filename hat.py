2768c7e453aef1296a706a87d68d83d76a13bc99
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-06 12:03:16 +0600

    added new file

--- hat.py
+++ hat.py
@@ -1,13 +1,12 @@
 #print(type({}))
 import random
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw","Slytherin"]
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw","Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
 
 
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
 
-
-hat = Hat()
-hat.sort("Harry")
+Hat.sort("Harry")
