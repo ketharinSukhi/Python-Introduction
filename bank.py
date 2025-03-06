93c3b306a7175699dc15c7a94381098425584e27
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-06 14:57:31 +0600

    added new file

--- bank.py
+++ bank.py
@@ -1,20 +1,28 @@
-balance = 0
+#Global variable
+class Account:
+    def __init__(self):
+        self._balance = 100
 
+
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self,n):
+        self._balance +=n
+        print("Total Balance: ", self.balance)
+
+    def withdraw(self, n):
+        self._balance -=n
 
 def main():
-    
-    print("Balance: ", balance)
-    deposit(100)
-    withdraw(75)
-    print("Balance: ", balance)
-
-def deposit(n):
-    global balance
-    balance +=n
-
-def withdraw(n):
-    global balance
-    balance -=n
+    account = Account()
+    print("Balance :", account.balance)
+    account.deposit(1000)
+    account.withdraw(700)
+    print("Balance:", account.balance)
 
 if __name__ =="__main__":
     main()
+
+
