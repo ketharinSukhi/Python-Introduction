e85fb4384c3707e0765f37e255d0e18e4dbcb89c
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-06 16:42:42 +0600

    added new file

--- unpack.py
+++ unpack.py
@@ -1,6 +1,13 @@
-def total(galleons, sickles, knuts):
+#Unpacking
+
+"""def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
 coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
-print(total(**coins), "Knuts") #unpacking
+print(total(**coins), "Knuts") #unpacking"""
+
+def f(*args, **kwargs):
+    print("Positional: ", kwargs)
+
+f(galleons=100, sickles=50, knuts=25)
