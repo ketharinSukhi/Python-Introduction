bee4acb6af2bb977c28d73d5260e5423173703b8
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-07 12:04:18 +0600

    added new folder

--- practice/first.py
+++ practice/first.py
@@ -1,6 +1,9 @@
-name = str(input("What is your name? \n"))
+"""name = str(input("What is your name? \n"))
 
 if str.isalpha(name):
  print("my name is", name)
 else:
- print("enter name")
+ print("enter name")"""
+
+name = input("what is your name? ")
+print("my name is", name)
