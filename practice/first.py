3afbcb8b8bcfd09e40dd1f2f446489b7825a52f0
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-09 10:05:52 +0600

    added new file

--- practice/first.py
+++ practice/first.py
@@ -5,13 +5,18 @@ if str.isalpha(name):
 else:
  print("enter name")"""
 
-def main(x, y):
-    x = int(input("what is x ?"))
-    print(square(x))
-
-def square():
-    x = x*x
-    return x
+def select(x, y):
+    if x >y:
+        print("x is greater than y")
+    elif x<y:
+        print("x is smaller than y")
+    else:
+        print("x is equal to y")
+def main():
+    x = input("what is x ?")
+    y = input("what is y ?")
+    select(x, y)
+   
 
 main()
  
