a02cbd451cae4a0d5910bf90a153b6409ec01f74
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-09 15:17:56 +0600

    added new file

--- loops.py
+++ loops.py
@@ -36,7 +36,7 @@ for i in range(len(students)):
     print(i+1, students[i])"""
 
 #dict
-"""students = [
+students = [
             {"name":"Hermione","house":"Gryffindor","patronus":"Otter"},
             {"name":"Harry","house":"Gryffindor","patronus":"Stag"},
             {"name":"Ron","house":"Gryffindor","patronus":"Jack Russel Terrier"},
@@ -44,10 +44,10 @@ for i in range(len(students)):
              ]
 
 for student in students:
-  print(student["name"], student["house"], student["patronus"], sep=" ----> ")"""
+  print(student["name"], student["house"], student["patronus"], sep=" ----> ")
 
 
-def main() :
+"""def main() :
     print_square(3)
 
 
@@ -63,5 +63,5 @@ def print_square(size) :
 def print_row(width):
     print("*"* width)
 
-main()
+main()"""
 
