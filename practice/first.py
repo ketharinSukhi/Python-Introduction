2174fae39ff9e56f91dd664be0eab74d45b5f49e
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-10 12:41:48 +0600

    ain

--- practice/first.py
+++ practice/first.py
@@ -35,7 +35,17 @@ base_url="https://pokeapi.co/api/v2/"
 def get_info(name):
     url =f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    print(response)
+    
+    if response.status_code == 200:
+        poke_data = response.json() #using jeson method we'll covert it to a python dictionary it will consist of key value pairs much like a jason file
+        return poke_data
+    else:
+        print(f"Failed to retrieve data{response.status_code}")
+        
 
 poke_name = "pikachu"
-get_info(poke_name) 
+poke_info = get_info(poke_name) 
+
+if poke_info:
+    print(f"{poke_info["name"]}")
+    
