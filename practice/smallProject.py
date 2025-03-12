85e761a15f8702d1be7283cee1d332985385332c
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-12 12:37:42 +0600

    added new file

--- practice/smallProject.py
+++ practice/smallProject.py
@@ -20,8 +20,8 @@ s1.get_average()"""
 
 #abstraction
 #encapsulation
-
-class Account:
+#bank account
+"""class Account:
     def __init__(self, bal, acc):
         self.balance = bal
         self.account = acc
@@ -38,11 +38,28 @@ class Account:
         print("credit amount",amount)
         print("total balance",self.balance)
 
-   
+    def get_balace(self):
+        return self.balance
         
         
 
 acc1 = Account(10000, 1234)
 acc1.debit(500)
-acc1.credit(1000)
+acc1.credit(1000)"""
+
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.account = acc_no
+        # to make it private use two under score before __
+        self.__password = acc_pass
+   
+    def reset_pass(self):
+        print(self.__password)
+
+acc1 = Account(12345,"abcd")
+
+print(acc1.account)
+print(acc1.reset_pass())
+
+        
 
