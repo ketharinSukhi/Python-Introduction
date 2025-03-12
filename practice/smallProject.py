e5999916a3ebcd16e273b8b636fc2217d800ca3c
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-12 11:03:54 +0600

    added new file

--- practice/smallProject.py
+++ practice/smallProject.py
@@ -1,8 +1,12 @@
-class Student:
+"""class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks =marks
-    
+
+    @staticmethod #decorator convert
+    def hello():
+        print("hello")
+
     def get_average(self):
         sum=0
         for i in self.marks:
@@ -12,4 +16,20 @@ class Student:
 
 
 s1 = Student("Tina",[67, 80, 95])
-s1.get_average()
+s1.get_average()"""
+
+#abstraction
+
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+    
+    def start(self):
+        self.clutch = True
+        self.acc =True
+        print("start the car")
+
+car1 = Car()
+car1.start()
