460a881b1f4e21fcd2c143d3c0512a839c91887a
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-13 09:47:06 +0600

    added new file

--- practice/set.py
+++ practice/set.py
@@ -1,9 +1,16 @@
 #set create
 
-set1 = {1,2,3}
+"""set1 = {1,2,3}
 set2 = {3,4,5}
 #create empty set
 #collection =set()
 
 print(set1.union(set2))
-print(set1.intersection(set2))
+print(set1.intersection(set2))"""
+
+x = 30
+
+def display():
+    print("Global x inside function:", globals()['x'])
+
+display()
