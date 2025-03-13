5c72342603225df7b46dc8dd105c75fe9ef92afa
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-13 15:45:35 +0600

    added new file

--- practice/Contact_Book_Application.py
+++ practice/Contact_Book_Application.py
@@ -7,38 +7,43 @@ class Book:
         
         self.name = book_name
 
-
-    def add():
-
-        print("Book is added in the list")
-
+    @staticmethod
+    def add(name):
+        with open("Book.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([name])
+        print(f"{name} Book is added in the list")
+    @staticmethod
     def update():
         print("Book is updated in the list")
-
+    @staticmethod
     def delete():
-        print("Book is deleted in the list")
-
+        print("Book is deleted from the list")
+    @staticmethod
     def search_contacts():
         print("Book is searching in the list")
-
+    @staticmethod
     def options(criteria):
       match criteria:
         case 1:
-             name = input("Enter the book name for add")
-             name = Book.add()
+             name = input("Enter the book name for add: ")
+             name = Book.add(name)
              return name
         case 2:
-            name = input("Enter the book name for update")
+            name = input("Enter the book name for update: ")
             name = Book.update()
             return name
         case 3:
-            name = input("Enter the book name for update")
+
+            name = input("Enter the book name for update: ")
             name = Book.delete()
             return name
         case 4:
-            name = input("Enter the book name for update")
+            name = input("Enter the book name for update: ")
             name = Book.search_contacts()
             return name
+        case _:
+              print("Invalid")
 
 def main():
    print("Choose:")
@@ -46,8 +51,7 @@ def main():
 
    criteria = int(input("Enter your number: "))
    Book.options(criteria)
-            
-        
+                    
 
 if __name__ == "__main__":
     main()
