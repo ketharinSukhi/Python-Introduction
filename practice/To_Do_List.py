d66e4f70d062be55d7b4a064aa38e1c5c2d037a1
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-14 10:25:27 +0600

    added new file

---
+++ practice/To_Do_List.py
@@ -0,0 +1,15 @@
+import csv
+class Task:
+    def __init__(self):
+        
+    def add_Tasks():
+
+     
+class ToDoList:
+    def __init__(self):
+        
+    
+
+if __name__ == '__main__':
+    main()
+        
