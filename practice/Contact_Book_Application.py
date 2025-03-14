82a196fa7314fc9d4180551c53a532742c57b743
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-14 09:22:28 +0600

    added new file

--- practice/Contact_Book_Application.py
+++ practice/Contact_Book_Application.py
@@ -43,30 +43,7 @@ class Book:
 
 
 
-    @staticmethod
-    def delete(name):
-        books = []
-        deleted = False
-
-        with open(Book.FILE_NAME, "r", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row and row[0] == name:
-                    deleted = True  # Skip this book (delete)
-                else:
-                    books.append(row)
-
-        if deleted:
-            with open(Book.FILE_NAME, "w", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerows(books)
-            print(f'"{name}" has been deleted from the list.')
-        else:
-            print(f'Book "{name}" not found.')
-        
-    @staticmethod
-    def search_contacts():
-        print("Book is searching in the list")
+   
     @staticmethod
     def options(criteria):
       match criteria:
@@ -83,12 +60,12 @@ class Book:
         case 3:
 
             name = input("Enter the book name for update: ")
-            name = Book.delete(name)
+            Book.delete(name)
             
         case 4:
-            name = input("Enter the book name for update: ")
-            name = Book.search_contacts()
-            
+            name = input("Enter the book name for search: ")
+            Book.search_contacts(name)
+             
         case _:
               print("Invalid")
 
