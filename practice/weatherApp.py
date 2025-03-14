00df907235fbe1e2fc01a93e277a979e02c357a6
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-14 12:25:11 +0600

    added new file

---
+++ practice/weatherApp.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import re
+
+def get_weather(city):
+
+    
+
+def save_to_file(city):
+
+
+def display_search():
+
+def main():
+   city = input ("Enter the city name: ")
+   get_weather(city)
+
+if __name__ == '__main__':
+    main()
+    
