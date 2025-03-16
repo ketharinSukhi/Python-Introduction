285865f0024c6c0fdb82bb4fdf670f897a7e51a9
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-16 15:26:49 +0600

    added new file

--- format.py
+++ format.py
@@ -1,17 +1,17 @@
-"""import re
+import re
 name = input("what's your name? ").strip()
 
 #:= wlrus 
 if matches:= re.search(r"^(.+), (.+)$", name):
     name = matches.group(2) + " " + matches.group(1)
-print(f"hello,{name}")"""
+print(f"hello,{name}")
 
 #regular expression
 #facebook URL
-import re
+"""import re
 url = input("URL : ").strip()
 
 if matches:=re.search(r"^https?://(?:www\.)?facebook\.com/(.+)$" , url, re.IGNORECASE):
-    print(f"Username:" , matches.group(1))
+    print(f"Username:" , matches.group(1))"""
 
 
