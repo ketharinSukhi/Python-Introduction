9aadcad4de68ba72b331db673d42a9ec94086081
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-17 09:24:40 +0600

    added new file

--- unpack.py
+++ unpack.py
@@ -1,11 +1,11 @@
 #Unpacking
 
-"""def total(galleons, sickles, knuts):
+def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
 coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
-print(total(**coins), "Knuts") #unpacking"""
+print(total(**coins), "Knuts") #unpacking
 
 #args and kwargs
 """def f(*args, **kwargs):
@@ -14,7 +14,7 @@ print(total(**coins), "Knuts") #unpacking"""
 f(galleons=100, sickles=50, knuts=25)"""
 
 #map and list comprehensions
-def main(): 
+"""def main(): 
     yell("This","is","CS50")
 
 def yell(*words):
@@ -24,4 +24,4 @@ def yell(*words):
 
 
 if __name__ == "__main__":
-     main()
+     main()"""
