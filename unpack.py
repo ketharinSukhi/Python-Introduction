6d528a460c1926ca5d10aa57b57a9fd312fcaaeb
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-17 10:04:42 +0600

    added new file

--- unpack.py
+++ unpack.py
@@ -7,21 +7,21 @@ coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
 print(total(**coins), "Knuts") #unpacking
 
-#args and kwargs
-def f(**kwargs):
+#arguments and keywords unpacking
+def f(*args, **kwargs):
     print("Positional: ", kwargs)
 
 f(galleons=100, sickles=50, knuts=25)
 
 #map and list comprehensions
-"""def main(): 
-    yell("This","is","CS50")
+def main(): 
+    call("This","is","CS50")
 
-def yell(*words):
+def call(*words):
     uppercased = [word.upper() for word in words]
     #uppercased = map(str.upper, words)
     print(*uppercased)
 
 
 if __name__ == "__main__":
-     main()"""
+     main()
