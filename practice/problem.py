165ba8b9bee9894407baf51891d5fdf2d53a639b
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-18 10:22:42 +0600

    added new file

--- practice/problem.py
+++ practice/problem.py
@@ -1,10 +1,24 @@
+#pyramid 1
+number = int(input("Enter the number : "))
 
+for i in range (1, number+1):
+    for j in range(i):
+        print("*", end="")
+    print(" ")
 
+#pyramid 2
+
+for i in range (1, number+1):
+    for j in range(i):
+        print(" o", end="")
+    print("*")
+
+print("\n\n")
 for i in range(1, 4):
     for j in range(1, 4):
         print(f"({i}, {j})", end=" ")
 
-print(" ")
+    print(" ")
 
 
 
