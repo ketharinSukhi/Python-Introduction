da64fce3f6af8af3b21c1ea9f5b677cbd7d4fa39
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-18 12:05:18 +0600

    added new file

--- practice/problem.py
+++ practice/problem.py
@@ -9,12 +9,21 @@ number = int(input("Enter the number : "))
 
 #pyramid 2
 
-for i in range (1, number+1):
+""""for i in range (1, number+1):
     for j in range(number-i):
         print(" ", end="")
     for m in range(i):
         print("*", end=" ")
+    print(" ")"""
+
+#pyramid 3
+num = number+1
+for i in range (1, num):
+    for j in range(num-i):
+        print("*", end="")
+    
     print(" ")
+    
     
 
 
