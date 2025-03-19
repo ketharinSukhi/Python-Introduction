49545374b08f0fde05e90c31e5987d8dfafc67b1
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-19 10:43:51 +0600

    added new file

--- practice/RockPaperScissors.py
+++ practice/RockPaperScissors.py
@@ -1,22 +1,30 @@
 import random
 print ("WELCOME TO ROCK PAPER SCISSOR GAME !!!")
 
-def computer_choice():
+def get_computer_choice():
     return random.choice(["rock","paper","scissor"])
 
-def user_choice():
+def get_user_choice():
    choice = input("Enter rock, paper or scissors: ")
-   while choice not in ["rock","paper","scissor"]:
+   while choice not in ["rock","paper","scissors"]:
         print("Invalid choice :(")
         choice = input("Enter rock, paper or scissors: ")
    return choice
 
 def winner(user, computer):
+    if user == computer:
+       return "It is a tie"
+    elif(user == "rock" and computer =="scissors") or \
+        (user == "paper" and computer == "rock") or \
+        (user == "scissors" and computer == "rock"):
+       return "YOU WIN !!"
 
+    else:
+       return "YOU LOST !!"
 
 def play_game():
-   user_choice = user_choice()
-   computer_choice = computer_choice()
+   user_choice = get_user_choice()
+   computer_choice = get_computer_choice()
    print(f"Computer Chose : {computer_choice}")
    print(winner(user_choice, computer_choice))
 
