f08833f9e078582b81d98545d9d60ef6ca5a0fb9
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-19 12:20:37 +0600

    added new file

--- Practice_1/GuessTheNumber.py
+++ Practice_1/GuessTheNumber.py
@@ -1,14 +1,14 @@
 import random
 
-def generate_random_number(start, end):
+def get_random_number(start, end):
 
     return random.randint(start, end)
 
 def play_game():
-    print("welcome to the game")
+    print("WELCOME TO NUMBER GUESSING GAME")
     #number range
     start, end = 1, 100
-    random_number = generate_random_number(start, end)
+    random_number = get_random_number(start, end)
     attempts = 0
 
     while True:
@@ -21,17 +21,19 @@ def play_game():
                 print(f"Guess a number between {start} and {end}:")
 
             elif guess < random_number:
-                print("Too low !!")
+                print(f"number was {random_number}. try again!!")
             elif guess > random_number:
-                print("Too high !!")
+                print(f"number was {random_number}. try again!!")
             else:
                 print(f"Congratulations! You guessed the number {random_number} in {attempts} attempts.")
                 break
         except ValueError:
             print("Invalid input! Please enter a valid number.")
 
-if __name__ == "__main__":
+def main():
     play_game()
+if __name__ == "__main__":
+    main()
 
 
         
