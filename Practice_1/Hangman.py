af9f79d379e90df9e5301ce91b2d2a80deb04c74
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-19 14:26:50 +0600

    added new file

--- Practice_1/Hangman.py
+++ Practice_1/Hangman.py
@@ -5,14 +5,20 @@ word_list = ["dog", "carrot","banana","apple"]
 def choose_word():
     return random.choice(word_list)
 
-def display_word():
+def display_word(word, guessed_letters):
+    return " ".join([letter if letter in guessed_letters else "_" for letter in word])
 
 def hangman():
     word_to_guess = choose_word()
-    guessed_letter = set()
+    guessed_letters = set()
     attempts =6
 
     print("WELCOME TO HANGMAN GAME !!")
+
+    while attempts >0:
+        print("\n" + display_word(word_to_guess, guessed_letters))
+        guess = input("Guess a letter: ").lower()
+
     
 
 def main():
