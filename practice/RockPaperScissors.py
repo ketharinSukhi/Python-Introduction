951e42ff3dde75fb602c66529e71aa67284e1ecb
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-19 09:35:44 +0600

    added new file

--- practice/RockPaperScissors.py
+++ practice/RockPaperScissors.py
@@ -1,8 +1,26 @@
 import random
 print ("WELCOME TO ROCK PAPER SCISSOR GAME !!!")
-Myself = input("Enter your choice : ")
-elements=random.choice(["Rock","Paper","Scissor"])
-print(elements)
+
+def computer_choice():
+    random.choice(["rock","paper","scissor"])
+
+def user_choice():
+
+
+
+def winner(user, computer):
+
+
+def play_game():
+
+def main():
+    play_game()
+if __name__ =="__main__":
+
+ main()
+
+
+
 
 
 
