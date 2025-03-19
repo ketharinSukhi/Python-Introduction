23186868d8aa61719998c736da8a475bf3edf37e
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-19 11:29:05 +0600

    added new file

--- Practice_1/MadLibs.py
+++ Practice_1/MadLibs.py
@@ -1,1 +1,13 @@
 print("WELCOME TO MAD LIBS GAME !!")
+def mad_lids():
+    adjective = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    place = input("Enter a place: ")
+    story = f"One day, a {adjective} {noun} decided to {verb} at the {place}. It was the best day ever!"
+    print("Here is mad lids story : " ,story)
+def main():
+    mad_lids()
+
+if __name__ == "__main__":
+    main()
