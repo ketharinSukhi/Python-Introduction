d94be74969fae06fa7967fc5e8a54445611b1eb8
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-20 09:45:23 +0600

    added new file

--- Practice_1/PasswordGenerator.py
+++ Practice_1/PasswordGenerator.py
@@ -4,5 +4,14 @@ def generate_password(length):
 
 def get_password():
 
+    try:
+        passwords = input("How many password you want to generate : ")
+        length = int(input("Enter the password length : "))
+
+        if length < 4:
+            
 
 def main():
+
+if __name__=="__main__":
+    main()
