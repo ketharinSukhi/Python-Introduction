05cf30b4aeaecbebb8fa838d00aa8fa9fd92d640
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-20 12:10:11 +0600

    added new file

--- Practice_1/MathGame.py
+++ Practice_1/MathGame.py
@@ -5,6 +5,17 @@ def generate_question():
    number2 = random.randint(1, 100)
    operator = random.choice(["+","-","*","/"])
 
+   if operator == "/":
+        number1 = number1 / number2
+
+   question = f"{number1}{operator}{number2}"
+   answer = eval(question)
+
+   return question, round(answer, 2)
+
+def math_game():
+    
+
 
 
 def main():
