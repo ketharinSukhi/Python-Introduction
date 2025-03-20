0f6c56a4488814f9496724a8c3480f32364eb976
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-20 15:32:32 +0600

    added new file

--- Practice_1/TicTacToe.py
+++ Practice_1/TicTacToe.py
@@ -4,11 +4,18 @@ def print_board(board):
     for row in board:
         print("   | ".join(row))
         print("-" * 15)
-
+def get_player_move():
+    while True:
+        try:
+            move = int(input("Enter your move (1-9): "))-1
+            if move<0 or move >= 9:
+                raise ValueError
+            return divmod(move, 3)
 
 def tic_tac_toe():
     board = [[" " for _ in range(3)] for _ in range(3)]
-
+    players = ["X", "O"]
+    current_player = random.choice(players)
     print("\nWELCOME TO TIC-TAC-TOE !\n")
     print_board(board)
 
