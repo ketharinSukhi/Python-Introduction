652d88c77a49beb43ab674b91d271c55fc17dadc
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-20 16:50:10 +0600

    added new file

--- Practice_1/TicTacToe.py
+++ Practice_1/TicTacToe.py
@@ -4,6 +4,20 @@ def print_board(board):
     for row in board:
         print("   | ".join(row))
         print("-" * 15)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    
+    return False 
        
 def get_player_move():
     while True:
@@ -31,6 +45,18 @@ def tic_tac_toe():
         else:
             print("That spot is already taken. Try again.")
             continue
+        print_board(board)
+        
+        if check_winner(board, current_player):
+            print(f"{current_player} wins!")
+            break
+        elif is_full(board):
+            print("It's a tie!")
+            break
+        
+        current_player = "X" if current_player == "O" else "O"
+    
+    print("Game Over! Thanks for playing.")
 
 
 def main():
