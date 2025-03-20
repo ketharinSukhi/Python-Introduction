cbaddac51c99cc76be66bf74aa8522b9c6c9724e
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-20 15:10:38 +0600

    added new file

--- Practice_1/TicTacToe.py
+++ Practice_1/TicTacToe.py
@@ -1,10 +1,19 @@
 import random
 
-def print_board():
+def print_board(board):
+    for row in board:
+        print("   | ".join(row))
+        print("-" * 15)
+
 
 def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+
+    print("\nWELCOME TO TIC-TAC-TOE !\n")
+    print_board(board)
 
 def main():
+    tic_tac_toe()
 
 if __name__ == "__main__":
     main()
