0b5ca1140d41372ae31214346db5f80f93093d7b
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-21 16:10:50 +0600

    added new file

--- Practice_1/StopWatch.py
+++ Practice_1/StopWatch.py
@@ -11,4 +11,14 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+# Initialize Screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Stopwatch")
+# Font
+font = pygame.font.Font(None, 50)
+button_font = pygame.font.Font(None, 30)
 
+# Stopwatch Variables
+start_time = 0
+elapsed_time = 0
+running = False
