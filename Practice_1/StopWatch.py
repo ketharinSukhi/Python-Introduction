db4df1266c16cb520b677483cce66fd409e08c5d
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-21 15:45:01 +0600

    added new file

---
+++ Practice_1/StopWatch.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 400, 300
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+
