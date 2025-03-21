ea12b4072080cd9b43716b8c50fff385eec3713f
KETHARIN SUKHI <ketharinn@gmail.com>
2025-03-21 10:25:07 +0600

    added new file

--- Practice_1/more.py
+++ Practice_1/more.py
@@ -5,7 +5,7 @@ pygame.init()
 
 #constants
 width, height = 600, 400
-cell_size = 20
+cell_size = 30
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
@@ -13,6 +13,10 @@ black = (0, 0, 0)
 
 #game window
 
-screen = pygame.display.set_mode(600, 400)
+screen = pygame.display.set_mode(width, height)
 pygame.display.set_caption("SNAKKE GAME")
+
+#snake initialization
+snake = [(100, 100), (90, 100), (80, 100)]
+snake_dir = (cell_size, 0)
 
